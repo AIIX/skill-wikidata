2c460d6fa29094b30243e29e53291461992602fd
Aditya Mehra <Aix.m@outlook.com>
2018-11-06 12:16:00 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# <img src='https://rawgithub.com/FortAwesome/Font-Awesome/master/advanced-options/raw-svg/solid/wikipedia-w.svg' card_color='#22a7f0' width='50' height='50' style='vertical-align:bottom'/> Wikidata
+Wikidate Person Information
+
+## About 
+Get Current and Historic Facts & Information About A Person From Wikidata
+
+## Examples 
+* "How old is steve jobs?"
+* "Where was bill gates born?"
+
+## Credits 
+Aix (@aiix)
+
+## Category
+**Entertainment**
+
+## Tags
+#wikidata
+#wikipedia
+#person
+#history
+#information
---
+++ __init__.py
@@ -0,0 +1,387 @@
+import re
+import sys
+import json
+import time
+from datetime import datetime
+import arrow
+import requests
+from os.path import dirname, join
+from bs4 import BeautifulSoup
+from adapt.intent import IntentBuilder
+from mycroft import MycroftSkill, intent_handler, intent_file_handler
+from mycroft.messagebus.message import Message
+
+zurl = "https://www.wikidata.org/entity/"
+entity_json = ""
+
+class WikidataSkill(MycroftSkill):
+    """ 
+    Wikidata Skill
+    """
+    @intent_file_handler('birth.place.of.person.intent')
+    def handle_wikidata_birthplace_intent(self, message):
+        """ 
+        Wikidata BirthPlace Intent
+        """
+        global entity_json
+        entity_id = self.getEntityId(message.data['person'])
+        person_name = message.data['person']
+        thumb_image = self.always_get_image(entity_id)
+        try:
+            get_entity_birth_place_id = entity_json['entities'][entity_id]['claims']['P19'][0]['mainsnak']['datavalue']['value']['id']
+            try:
+                get_entity_birth_place_response = requests.get(zurl+get_entity_birth_place_id)
+                entity_birth_place_json =  get_entity_birth_place_response.json()
+                entity_birth_place = entity_birth_place_json['entities'][get_entity_birth_place_id]['labels']['en']['value']
+                result_speak = "{0} was born in {1}".format(person_name, entity_birth_place)
+                self.speak(result_speak)
+                self.enclosure.bus.emit(Message("metadata", {"type": "skill-wikidata", "personContext": person_name,"answerData": result_speak, "imgLink": thumb_image}))
+            except Exception as e:
+                notFoundMessage = "I couldn't find where {0} was born".format(person_name)
+                self.speak(notFoundMessage)
+                entity_birth_place = False
+        except:
+            notFoundMessage = "I couldn't find where {0} was born".format(person_name)
+            self.speak(notFoundMessage)
+            get_entity_birth_place_id = False
+
+    @intent_file_handler('death.place.of.person.intent')
+    def handle_wikidata_deathplace_intent(self, message):
+        """ 
+        Wikidata DeathPlace Intent
+        """        
+        global entity_json
+        entity_id = self.getEntityId(message.data['person'])
+        person_name = message.data['person']
+        thumb_image = self.always_get_image(entity_id)
+        try:
+            get_entity_death_place_id = entity_json['entities'][entity_id]['claims']['P20'][0]['mainsnak']['datavalue']['value']['id']
+            try:
+                get_entity_death_place_response = requests.get(zurl+get_entity_death_place_id)
+                entity_death_place_json = get_entity_death_place_response.json()
+                entity_death_place = entity_death_place_json['entities'][get_entity_death_place_id]['labels']['en']['value']
+                result_speak = "{0} died in {1}".format(person_name, entity_death_place)
+                self.speak(result_speak)
+                self.enclosure.bus.emit(Message("metadata", {"type": "skill-wikidata", "personContext": person_name, "answerData": result_speak, "imgLink": thumb_image}))
+            except Exception as e:
+                notFoundMessage = "I couldn't find where {0} died".format(person_name)
+                self.speak(notFoundMessage)
+                entity_death_place = False
+        except:
+            notFoundMessage = "I couldn't find where {0} died".format(person_name)
+            self.speak(notFoundMessage)
+            get_entity_death_place_id = False
+
+    @intent_file_handler('how.old.is.person.intent')
+    def handle_wikidata_age_intent(self, message):
+        """ 
+        Wikidata SearchAge Intent
+        """        
+        global entity_json
+        entity_id = self.getEntityId(message.data['person'])
+        person_name = message.data['person']
+        thumb_image = self.always_get_image(entity_id)
+        try:
+            get_entity_dob_id = entity_json['entities'][entity_id]['claims']['P569'][0]['mainsnak']['datavalue']['value']['time']
+            try:
+                a = arrow.get(get_entity_dob_id)
+                birth_date = a.format("DD/MM/YYYY")
+                dob = birth_date
+                dob = datetime.strptime(dob, '%d/%m/%Y')
+                age = ("%d" % ((datetime.today() - dob).days/365))
+                result_msg = "The age of {0} is {1}".format(person_name, age)
+                self.speak(result_msg)
+                self.enclosure.bus.emit(Message("metadata", {"type": "skill-wikidata", "personContext": person_name, "answerData": result_msg, "imgLink": thumb_image}))
+            except Exception as e:
+                notFoundMessage = "I couldn't find the age of {0}".format(person_name)
+                self.speak(notFoundMessage)
+                entity_death_place = False
+        except:
+            notFoundMessage = "I couldn't find the age of {0}".format(person_name)
+            self.speak(notFoundMessage)
+            get_entity_death_place_id = False
+
+    @intent_file_handler('what.is.the.gender.intent')
+    def handle_wikidata_gender_intent(self, message):
+        """ 
+        Wikidata Gender Intent
+        """
+        global entity_json
+        entity_id = self.getEntityId(message.data['person'])
+        person_name = message.data['person']
+        thumb_image = self.always_get_image(entity_id)
+        try:
+            get_entity_gender_id = entity_json['entities'][entity_id]['claims']['P21'][0]['mainsnak']['datavalue']['value']['id']
+            try:
+                get_entity_gender_id_response = requests.get(zurl+get_entity_gender_id)
+                entity_gender_id_json =  get_entity_gender_id_response.json()
+                entity_gender = entity_gender_id_json['entities'][get_entity_gender_id]['labels']['en']['value'] 
+                self.speak(entity_gender)
+                self.enclosure.bus.emit(Message("metadata", {"type": "skill-wikidata", "personContext": person_name,"answerData": entity_gender, "imgLink": thumb_image}))
+            except Exception as e:
+                notFoundMessage = "I couldn't find the gender of {0}".format(person_name)
+                self.speak(notFoundMessage)
+                entity_gender_id = False
+        except:
+            notFoundMessage = "I couldn't find the gender of {0}".format(person_name)
+            self.speak(notFoundMessage)
+            get_entity_gender_id = False
+    
+    @intent_file_handler('who.is.the.spouse.intent')
+    def handle_wikidata_spouse_intent(self, message):
+        """
+        Wikidata Spouse Intent
+        """
+        global entity_json
+        entity_id = self.getEntityId(message.data['person'])
+        person_name = message.data['person']
+        thumb_image = self.always_get_image(entity_id)
+        try:
+            get_entity_spouse_id = entity_json['entities'][entity_id]['claims']['P26'][0]['mainsnak']['datavalue']['value']['id']
+            try:
+                get_entity_spouse_id_response = requests.get(zurl+get_entity_spouse_id)
+                entity_spouse_id_json = get_entity_spouse_id_response.json()
+                entity_spouse_id = entity_spouse_id_json['entities'][get_entity_spouse_id]['labels']['en']['value']
+                self.speak(entity_spouse_id)
+                self.enclosure.bus.emit(Message("metadata", {"type": "skill-wikidata", "personContext": person_name,"answerData": entity_spouse_id, "imgLink": thumb_image}))
+            except Exception as e:
+                notFoundMessage = "I couldn't find any spouse of {0}".format(person_name)
+                self.speak(notFoundMessage)
+                entity_spouse_id = False
+        except:
+            notFoundMessage = "I couldn't find any spouse of {0}".format(person_name)
+            self.speak(notFoundMessage)
+            get_entity_spouse_id = False
+    
+    @intent_file_handler('which.country.is.from.intent')
+    def handle_wikidata_country_citizenship(self, message):
+        """
+        Wikidata Country Citizenship
+        """        
+        global entity_json
+        entity_id = self.getEntityId(message.data['person'])
+        person_name = message.data['person']
+        thumb_image = self.always_get_image(entity_id)
+        try:
+            get_entity_country_citizenship_id = entity_json['entities'][entity_id]['claims']['P27'][0]['mainsnak']['datavalue']['value']['id']
+            try:
+                get_entity_country_citizenship_id_response = requests.get(zurl+get_entity_country_citizenship_id)
+                entity_country_citizenship_json = get_entity_country_citizenship_id_response.json()
+                entity_country_citizenship = entity_country_citizenship_json['entities'][get_entity_country_citizenship_id]['labels']['en']['value']
+                self.speak(entity_country_citizenship)
+                self.enclosure.bus.emit(Message("metadata", {"type": "skill-wikidata", "personContext": person_name,"answerData": entity_country_citizenship, "imgLink": thumb_image}))
+            except Exception as e:
+                notFoundMessage = "I couldn't find which country {0} is from".format(person_name)
+                self.speak(notFoundMessage)
+                entity_country_citizenship = False
+        except:
+            notFoundMessage = "I couldn't find which country {0} is from".format(person_name)
+            self.speak(notFoundMessage)
+            get_entity_country_citizenship_id = False
+    
+    @intent_file_handler('what.children.name.intent')
+    def handle_wikidata_child_name(self, message):
+        """
+        Wikidata Child Name
+        """
+        global entity_json
+        entity_id = self.getEntityId(message.data['person'])
+        person_name = message.data['person']
+        thumb_image = self.always_get_image(entity_id)         
+        try:
+            get_entity_child_id = entity_json['entities'][entity_id]['claims']['P40'][0]['mainsnak']['datavalue']['value']['id']
+            try:
+                get_entity_child_id_response = requests.get(zurl+get_entity_child_id)
+                entity_child_id_json = get_entity_child_id_response.json()
+                entity_child_id = entity_child_id_json = entity_child_id_json['entities'][get_entity_child_id]['labels']['en']['value']
+                self.speak(entity_child_id)
+                self.enclosure.bus.emit(Message("metadata", {"type": "skill-wikidata", "personContext": person_name,"answerData": entity_child_id, "imgLink": thumb_image}))
+            except Exception as e:
+                notFoundMessage = "I couldn't find any children of {0}".format(person_name)
+                self.speak(notFoundMessage)
+                entity_child_id = False
+        except:
+            notFoundMessage = "I couldn't find any children of {0}".format(person_name)
+            self.speak(notFoundMessage)
+            get_entity_child_id = False
+            
+    @intent_file_handler('what.occupation.intent')
+    def handle_wikidata_occupation(self, message):
+        """
+        Wikidata Occupation
+        """
+        global entity_json
+        entity_id = self.getEntityId(message.data['person'])
+        person_name = message.data['person']
+        thumb_image = self.always_get_image(entity_id)        
+        try:
+            get_entity_occupation_id = entity_json['entities'][entity_id]['claims']['P106'][0]['mainsnak']['datavalue']['value']['id']
+            try:
+                get_entity_occupation_id_response = requests.get(zurl+get_entity_occupation_id)
+                entity_occupation_id_json = get_entity_occupation_id_response.json()
+                entity_occupation_id = entity_occupation_id_json['entities'][get_entity_occupation_id]['labels']['en']['value']
+                self.speak(entity_occupation_id)
+                self.enclosure.bus.emit(Message("metadata", {"type": "skill-wikidata", "personContext": person_name,"answerData": entity_occupation_id, "imgLink": thumb_image}))
+            except Exception as e:
+                notFoundMessage = "I couldn't find any occupation for {0}".format(person_name)
+                self.speak(notFoundMessage)
+                entity_occupation_id = False
+        except:
+            notFoundMessage = "I couldn't find any occupation for {0}".format(person_name)
+            self.speak(notFoundMessage)
+            get_entity_occupation_id = False
+    
+    @intent_file_handler('burial.location.intent')
+    def handle_wikidata_place_of_burial(self, message):
+        """
+        Wikidata Place Of Burial
+        """
+        global entity_json
+        entity_id = self.getEntityId(message.data['person'])
+        person_name = message.data['person']
+        
+        try:
+            get_entity_place_of_burial_id = entity_json['entities'][entity_id]['claims']['P119'][0]['mainsnak']['datavalue']['value']['id']
+            try:
+                get_entity_place_of_burial_id_response = requests.get(zurl+get_entity_place_of_burial_id)
+                entity_place_of_burial_id_json = get_entity_place_of_burial_id_response.json()
+                entity_place_of_burial_id = entity_place_of_burial_id_json['entities'][get_entity_place_of_burial_id]['labels']['en']['value']
+                self.speak(entity_place_of_burial_id)
+                self.enclosure.bus.emit(Message("metadata", {"type": "skill-wikidata", "personContext": person_name,"answerData": entity_place_of_burial_id, "imgLink": thumb_image}))
+            except Exception as e:
+                notFoundMessage = "I couldn't find any burial information for {0}".format(person_name)
+                self.speak(notFoundMessage)
+                entity_place_of_burial_id = False
+        except:
+            notFoundMessage = "I couldn't find any burial information for {0}".format(person_name)
+            self.speak(notFoundMessage)
+            get_entity_place_of_burial_id = False
+
+    @intent_file_handler('date.of.birth.intent')
+    def handle_wikidata_date_of_birth(self, message):
+        """
+        Wikidata Date of Birth
+        """
+        global entity_json
+        entity_id = self.getEntityId(message.data['person'])
+        person_name = message.data['person']
+        thumb_image = self.always_get_image(entity_id)        
+        try:
+            get_entity_dob_id = entity_json['entities'][entity_id]['claims']['P569'][0]['mainsnak']['datavalue']['value']['time']
+            try:
+                a = arrow.get(get_entity_dob_id)
+                birth_date = a.format("DD,MM,YYYY")
+                birth_date_breakup = birth_date.split(",")
+                person_name = message.data['person']
+                day = birth_date_breakup[0]
+                month = birth_date_breakup[1]
+                year = birth_date_breakup[2]
+                birth_date_to_speak = "{0} was born on day {1} of month {2} in the year {3}".format(person_name, day, month, year)
+                self.speak(birth_date_to_speak)
+                self.enclosure.bus.emit(Message("metadata", {"type": "skill-wikidata", "personContext": person_name,"answerData": birth_date, "imgLink": thumb_image}))
+            except Exception as e:
+                notFoundMessage = "I couldn't find birth date information for {0}".format(person_name)
+                self.speak(notFoundMessage)
+                entity_dob = False
+        except:
+            notFoundMessage = "I couldn't find birth date information for {0}".format(person_name)
+            self.speak(notFoundMessage)
+            get_entity_dob_id = False
+
+    @intent_file_handler('date.of.death.intent')
+    def handle_wikidata_date_of_death(self, message):
+        """
+        Wikidata Date of Death
+        """
+        global entity_json
+        entity_id = self.getEntityId(message.data['person'])
+        person_name = message.data['person']
+        thumb_image = self.always_get_image(entity_id)        
+        try:
+            get_entity_dod_id = entity_json['entities'][entity_id]['claims']['P570'][0]['mainsnak']['datavalue']['value']['time']
+            try:
+                a = arrow.get(get_entity_dod_id)
+                death_date = a.format("DD,MM,YYYY")
+                death_date_breakup = death_date.split(",")
+                person_name = message.data['person']
+                day = death_date_breakup[0]
+                month = death_date_breakup[1]
+                year = death_date_breakup[2]
+                death_date_to_speak = "{0} died on day {1} of month {2} in the year {3}".format(person_name, day, month, year)
+                self.speak(death_date_to_speak)
+                self.enclosure.bus.emit(Message("metadata", {"type": "skill-wikidata", "personContext": person_name,"answerData": death_date, "imgLink": thumb_image}))
+            except Exception as e:
+                notFoundMessage = "I couldn't find any death date information for {0}".format(person_name)
+                self.speak(notFoundMessage)
+                entity_dod = False
+        except:
+            notFoundMessage = "I couldn't find any death date information for {0}".format(person_name)
+            self.speak(notFoundMessage)
+            get_entity_dob_id = False
+    
+    def getAge(self, dateofbirth):
+        d = datetime.strptime(s, '%d %B, %Y')
+        print(d.strftime('%Y-%m-%d'))
+        return d.strftime('%Y-%m-%d')
+
+    def get_info_wikidata(self, datavalue):
+        name = datavalue.strip()
+        wikidataObject = self._wikidata(name)        
+        r = wikidataObject['search'][0]['concepturi']
+        return r
+        
+    def _wikidata(self, name):
+        url = "http://www.wikidata.org/w/api.php"
+        params = {
+            "search": name,
+            "action": "wbsearchentities",
+            "format": "json",
+            "language": "en",
+            "type": "item",
+            "continue": "0",
+            "limit": "10"
+        }
+        return requests.get(url, params=params).json()
+
+
+    def getEntityId(self, itemcontext):
+        item = itemcontext 
+        global entity_json
+        try:
+            result = self.get_info_wikidata(item)
+            if result != None:
+                entity_result = requests.get(result)
+                entity_json = entity_result.json()
+                for key in entity_json['entities'].keys():
+                    entity_id = key
+                    return entity_id
+        except:
+            return False
+
+    def always_get_image(self, datakey):
+            global entity_json
+            entity_id = datakey
+            try:
+                get_entity_pic_link = entity_json['entities'][entity_id]['claims']['P18'][0]['mainsnak']['datavalue']['value']
+                get_entity_pic = get_entity_pic_link.replace(" ", "_")
+                get_entity_pic_hyperlink = "https://commons.wikimedia.org/wiki/File:{0}".format(get_entity_pic)
+                get_entity_pic_content = requests.get(get_entity_pic_hyperlink)
+                soup = BeautifulSoup(get_entity_pic_content.content, 'html.parser')
+                imagelist = []
+                for link in soup.find_all('div', class_="fullImageLink"):
+                    for x in link.find_all('a'):
+                        r = x.get('href')
+                        imagelist.append(r)
+                
+                return imagelist[0]
+            except:
+                get_entity_pic_link = False
+                print(get_entity_pic_link)
+                return False
+
+    def stop(self):
+        pass
+
+
+def create_skill():
+    return WikidataSkill()
